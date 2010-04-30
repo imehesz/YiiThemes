8517c515ba905dff64508fd44054fab80965ad8b
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-04-30 13:11:27 -0400

    added google tracking for the site

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -176,4 +176,15 @@
   		<a href="http://yiiframework.com" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/yii_power_lightgrey_white.gif" /></a>
 	</div>
 </body>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-5417349-5");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
 </html>
