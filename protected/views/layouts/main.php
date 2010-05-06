7cc6992f2e4c4094189d35fbf446390c96b4511f
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-05-06 10:12:53 -0400

    added cool logo, i created it myself

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,8 @@
 	<?php /*<a href="index.html"><img src="images/105750.JPG" alt="Education Zone" width="245" height="37" border="0"  class="logo" title="Education Zone"/></a> */ ?>
 	<?php $images = Array('105750.JPG', 'fashion_mini_1.jpg', 'fashion_mini_2.gif', 'fashion_mini_3.jpg' );?>
 	<a href="http://en.wikipedia.org/wiki/Fashion" target="_blank"><img src="<?php Yii::app()->request->baseUrl;?>/images/<?php echo $images[rand(0,sizeof($images)-1)];?>" alt="Education Zone" border="0"  class="logo" title="Education Zone"/></a>
-	<p>Yii Themes - <span style="font-style:italic;">"your first step to be ridiculously good looking</span>"</p>
+    <a href="<?php echo Yii::app()->request->baseUrl;?>"><img src="<?php echo Yii::app()->request->baseUrl;?>/images/yiithemes_logo.png" style="float:left;" border="0"/></a>
+	<p><span style="font-style:italic;">"your first step to be ridiculously good looking</span>"</p>
 		
   <div id="topContactPan">
   </div>
