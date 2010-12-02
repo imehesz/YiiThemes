c0ac7e59d76bb798bb21a7baf67dbaced0c9b858
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-12-02 12:34:00 -0500

    added AddThis social sharing support - yaaaay

--- protected/views/theme/view.php
+++ protected/views/theme/view.php
@@ -121,6 +121,13 @@ COMMENTS;
 </div>
 </div>
 
+<div>
+    <!-- AddThis Button BEGIN -->
+    <a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4cf7d7190e31e605"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4cf7d7190e31e605"></script>
+    <!-- AddThis Button END -->
+</div>
+<p></p>
 <h2>Download Theme</h2>
 <?php if( $model->file ) : ?>
     <div>
