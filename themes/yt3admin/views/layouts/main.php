6798bf60fcd5fa1828630badba6c1262819f9bef
Imre Mehesz <imehesz@gmail.com>
2013-02-18 20:56:01 -0500

    added new admin theme, started admin pages

---
+++ themes/yt3admin/views/layouts/main.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en" ng-app="myApp">
+<head>
+  <meta charset="utf-8">
+  <title ng-bind-template="Yii Themes {{'- ' + pageTitle || ''}}"></title>
+  <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/reset.css"/>
+  <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap.min.css"/>
+  <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap-responsive.min.css"/>
+  <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootswatch.css"/>
+</head>
+<body>
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="#">YT 3.0</a>
+        <div class="nav-collapse" id="main-menu">
+          <ul class="nav" id="main-menu-left">
+            <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/theme/mythemes" title="List all my themes">My Themes</a></li>
+            <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/theme/create" title="Create a new theme">Create</a></li>
+          </ul>
+          <ul class="nav pull-right" id="main-menu-right">
+            <li>
+              <a ng-hide="userInfo.is_guest" href="<?php echo Yii::app()->request->baseUrl; ?>/user/user/logout"><?php echo Yii::app()->user->name ;?> - logout</a>
+            </li>
+          </ul>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .navbar-inner -->
+  </div> <!-- .navbar .navbar-fixed-top -->
+
+  <div class="container">
+    <?php echo $content; ?>
+  </div> <!-- .container -->
+
+</body>
+</html>
