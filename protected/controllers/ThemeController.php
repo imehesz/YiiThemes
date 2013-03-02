33a788da6c9676954bb960d6082683ff0d3225d9
Imre Mehesz <imehesz@gmail.com>
2013-03-02 14:05:26 -0500

    added ThemeBootstrap model, cleaned up and updated the Theme controller to dynamically handle models requests

--- protected/controllers/ThemeController.php
+++ protected/controllers/ThemeController.php
@@ -1,6 +1,6 @@
 <?php
 
-class ThemeController extends ERestController {
+class ThemeController extends Controller {
 	/**
 	 * @var string the default layout for the views. Defaults to 'column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
@@ -14,6 +14,8 @@ class ThemeController extends ERestController {
 
     public $nestedModels = false;
 
+    public $modelClass = null;
+
 	/**
 	 * @var CActiveRecord the currently loaded data model instance.
 	 */
@@ -21,23 +23,25 @@ class ThemeController extends ERestController {
 
 	public function beforeAction( $action )
 	{
+    if ( isset( Yii::app()->params['themeModel'] ) ) {
+      switch( Yii::app()->params['themeModel'] ) {
+        case "ThemeBootstrap": 
+          $this->modelClass = ThemeBootstrap::model();
+          break;
+      }
+    }
 
-		return parent::beforeAction( $action );
+    if ( ! $this->modelClass ) {
+      $this->modelClass = Theme::model();
+    }
 
-		// TODO fix this, maybe update Yii to a newer version?
-		$id = Yii::app()->request->getParam( 'id', null );
-
-		if( $id ) { $model = $this->loadModel(); }
-		else { $model = new Theme; }
-
-		Controller::$RIGHT_SIDEBAR = $this->renderPartial( 'application.components.views._right_sidebar_main', array( 'model' => $model ), true );
 		return parent::beforeAction( $action );
 	}
 
 	/**
 	 * @return array action filters
 	 */
-	public function _filters()
+	public function filters()
 	{
 		return array(
 			'accessControl', // perform access control for CRUD operations
@@ -50,7 +54,7 @@ class ThemeController extends ERestController {
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
 	 */
-	public function _accessRules()
+	public function accessRules()
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
@@ -98,33 +102,6 @@ class ThemeController extends ERestController {
     $this->render('view',array( 'model' => $model ));
   }
 
-	/**
-	 * Displays a particular model.
-	 */
-	public function actionView_before_yt3_theme()
-	{
-        // we add +1 to the view column ...
-        $model = $this->loadModel();
-        $model->setAttribute( 'viewed', $model->viewed+1 );
-        $model->skipUpdated = true;
-        
-        $model->save();
-
-        $this->pageTitle = 'Yii Themes - ' . $model->name;
-
-        // we need to find the NEXT and the PREVIOUS theme
-		// so we can create cool links ...
-		$next_theme = Theme::model()->find( 'id>' . $model->id . ' ORDER BY id' );
-		$prev_theme = Theme::model()->find( 'id<' . $model->id . ' ORDER BY id DESC' );
-
-		$this->render('view',array(
-			'model'			=> $model,
-			'wikiext' 		=> new wikiext(),
-			'next_theme'	=> $next_theme,
-			'prev_theme'	=> $prev_theme
-		));
-	}
-
   public function actionMythemes() {
     Yii::app()->theme = 'yt3admin';
 
@@ -135,7 +112,7 @@ class ThemeController extends ERestController {
 
     $this->render( 'mythemes', array(
       'themes' => new CActiveDataProvider( 
-        'Theme', 
+        get_class( $this->modelClass ), 
         array( 
           'criteria' => $criteria, 
           'pagination' => array( 'pageSize' => 15 )
@@ -151,14 +128,14 @@ class ThemeController extends ERestController {
 	public function actionCreate()
 	{
     Yii::app()->theme = 'yt3admin';
-		$model=new Theme;
+		$model=new $this->modelClass;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Theme']))
+		if(isset($_POST[get_class( $this->modelClass )]))
 		{
-			$model->attributes=$_POST['Theme'];
+			$model->attributes=$_POST[ get_class( $this->modelClass ) ];
 			
 			// TODO make this prettier here
 			// we're gonna create 3 random names for the files ...
@@ -197,9 +174,9 @@ class ThemeController extends ERestController {
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Theme']))
+		if(isset($_POST[ get_class( $this->modelClass ) ]))
 		{
-			$model->attributes=$_POST['Theme'];
+			$model->attributes=$_POST[ get_class( $this->modelClass ) ];
 
             if( $model->validate() )
             {
@@ -324,7 +301,7 @@ class ThemeController extends ERestController {
     $sort->applyOrder( $criteria );
 
     $dataProvider=new CActiveDataProvider(
-        'Theme', 
+        get_class( $this->modelClass ), 
         array( 
           'criteria' 		=> $criteria,
           'pagination' 	=> array( 'pageSize' => $page_size ),
@@ -340,6 +317,7 @@ class ThemeController extends ERestController {
 	/**
 	 * Manages all models.
 	 */
+   /*
 	public function actionAdmin()
 	{
 		$model=new Theme('search');
@@ -350,6 +328,7 @@ class ThemeController extends ERestController {
 			'model'=>$model,
 		));
 	}
+  */
 
     public function actionDownload()
     {
@@ -433,7 +412,7 @@ class ThemeController extends ERestController {
 		if($this->_model===null)
 		{
 			if(isset($_GET['id']))
-				$this->_model=Theme::model()->findbyPk($_GET['id']);
+				$this->_model = $this->modelClass->findbyPk($_GET['id']);
 			if($this->_model===null)
 				throw new CHttpException(404,'The requested page does not exist.');
 		}
@@ -454,149 +433,5 @@ class ThemeController extends ERestController {
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
-	}
-
-  ///////////////////////
-  // Custom REST stuff //
-  ///////////////////////
-
-  // TODO quick hack for now, so we can have user info
-  // constantly the same way ...
-  public function doCustomRestGetUserinfo() {
-    $this->outputHelper( 'Userinfo', null, 0 );
-  }
-
-  public function doRestDelete($id) {
-    $this->renderJson( array("message" => "Not supported action!") );
-  }
-
-  public function doRestUpdate($id, $data) {
-    $this->renderJson( array("message" => "Not supported action!") );
-  }
-
-  /**
-   * TODO there is a bug where if the user id is 1, filter returns 
-   * every author with  ID containing the number 1. ie: 1, 11, 121, 541 etc
-   */
-  public function doRestList() {
-    // first let's get the models
-
-    // if we have a filter, we might wanna look for 
-    // the author, so we need to change some things.
-    if ($this->restFilter ) {
-      $filters = CJSON::decode( $this->restFilter );
-      $new_filters = array();
-      foreach ( $filters as $filter ) {
-        if ( isset( $filter['property'] ) && $filter['property'] == 'author' ) {
-          // we got an author, let's deal with it ...
-          $author = $filter['value'];
-          if ( $author ) {
-            $user = User::model()->findByAttributes(array('username' => $author ));
-            if ( $user ) {
-              $new_filters[] = array( 'property' => 'userId', 'value' => $user->id );
-            }
-          }
-        } elseif ( isset( $filter['property'] ) && $filter['value'] ) {
-          $new_filters[] = $filter;
-        }
-      }
-      $this->restFilter = CJSON::encode( $new_filters );
-    }
-
-    $models = $this->getModel()->filter($this->restFilter)->orderBy($this->restSort)->limit($this->restLimit)->offset($this->restOffset)->findAll();
-
-    foreach ( $models as $model ) {
-      $jsonTheme = new StdClass();
-      $jsonTheme->id        = $model->id;
-      $jsonTheme->name      = $model->name;
-      $jsonTheme->artist    = $model->user->username;
-      $jsonTheme->image     = $model->preview1;
-      $jsonTheme->created   = $model->created;
-      $jsonTheme->updated   = $model->updated;
-      $jsonTheme->short_desc= $model->short_desc;
-      $retarr[] = $jsonTheme;
-    }
-
-    $count = $this->getModel()->filter( $this->restFilter )->count();
-    $this->outputHelper( 'Themes found', $retarr, $count );
-  }
-
-  public function doRestView( $id ) {
-    $theme = Theme::model()->with('user')->findByPk( $id );
-
-    if ( $theme ) {
-      $md = new CMarkdown;
-      $md->cssFile = false;
-
-      // let's update the visitors count ...
-      $theme->setAttribute( 'viewed', $theme->viewed+1 );
-      $theme->skipUpdated = true;
-      $theme->save();
-
-      $jsonTheme = new StdClass();
-      $jsonTheme->id        = $theme->id;
-      $jsonTheme->name      = $theme->name;
-      $jsonTheme->artist    = $theme->user->username;
-      $jsonTheme->view_cnt      = $theme->viewed;
-      $jsonTheme->download_cnt  = $theme->downloaded;
-      $jsonTheme->image     = $theme->preview1;
-      $jsonTheme->created   = $theme->created;
-      $jsonTheme->updated   = $theme->updated;
-      $jsonTheme->short_desc= $theme->short_desc;
-      $jsonTheme->long_desc = $md->transform( $theme->long_desc );
-      $jsonTheme->downloadable = ThemeUser::model()->canDownloadByIp( $id );
-
-      $this->outputHelper( 'Theme found', $jsonTheme, 1 );
-    }
-  }
-
-  public function doCustomRestGetRandomfive() {
-		$themes = Theme::model()->findAllByAttributes( array('deleted'=>0  ), array( 'order'=> 'rand()', 'limit'=>5 ) ); 
-    $retarr = array();
-
-    if ( ! empty( $themes ) ) {
-      foreach ( $themes as $theme ) {
-        $jsonTheme          = new StdClass();
-        $jsonTheme->id      = $theme->id;
-        $jsonTheme->name    = $theme->name;
-        $jsonTheme->short_desc    = $theme->short_desc;
-        $jsonTheme->view_cnt      = $theme->viewed;
-        $jsonTheme->download_cnt  = $theme->downloaded;
-        $jsonTheme->image         = $theme->preview1;
-        $jsonTheme->created       = $theme->created;
-        $jsonTheme->updated       = $theme->updated;
-        $jsonTheme->artist        = $theme->user->username;
-        $retarr[] = $jsonTheme;
-      }
-    }
-
-    $this->outputHelper( 'Random five themes', $retarr, sizeof( $retarr ) );
-  }
-
-	public function outputHelper($message, $results, $totalCount=0, $model=null)
-	{
-		if(is_null($model)) {
-			$model = lcfirst(get_class($model));
-    }
-		else {
-			$model = lcfirst($model);
-    }
-
-    // let's see if we know anything about the current user ...
-    $user_info = new StdClass();
-    $user_info->is_guest = Yii::app()->user->isGuest;
-    if ( ! $user_info->is_guest ) {
-      $user_info->name = Yii::app()->user->name;
-    }
-		
-		$this->renderJson(array(
-			'success'=>true, 
-      'user_info' => $user_info,
-			'message'=>$message, 
-			'data'=>array(
-				'totalCount'=>$totalCount, 
-				'themes'=>$results
-			)
-		));
 	}
 }
