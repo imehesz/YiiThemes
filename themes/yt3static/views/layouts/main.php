606dd85db79004b147cc7a21a898237f89675a9a
Imre Mehesz <imehesz@gmail.com>
2013-03-02 14:09:00 -0500

    updated admin title to respect app name

--- themes/yt3static/views/layouts/main.php
+++ themes/yt3static/views/layouts/main.php
@@ -63,7 +63,7 @@
 
     <footer id="footer">
       <p>
-        <div class="label pull-right">v 3.0.<span id='VERSION_ID'>2013.0301.2124</span></div>
+        <div class="label pull-right">v 3.0.<span id='VERSION_ID'>2013.0302.1408</span></div>
       </p>
       <div class="links">
         <a href="<?php echo $this->createUrl('/site/index'); ?>">Home</a>
