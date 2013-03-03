24dbbacf108469a6238f54fc61245cc83a6eed99
Imre Mehesz <imehesz@gmail.com>
2013-03-02 21:34:30 -0500

    fixed responsiveness in the yii theme

--- themes/yt3static/views/site/index.php
+++ themes/yt3static/views/site/index.php
@@ -1,52 +1,69 @@
 <?php $this->pageTitle = 'Home'; ?>
 <div class="row">
    
-  <?php if ( time() < 1363323600 ) : ?>
+  <?php if ( time() < 1363314761 ) : ?>
     <div class="span12">
       <p class="alert alert-info">
         <strong>Welcome</strong> to the re-imagined Yii Themes, now called <strong>Yii Theme Factory</strong>, the largest and the only <strong>FREE</strong> source of quality web designs for the Yii (PHP) Framework. Please <a href="<?php $this->createUrl( '/user/user/registration' ); ?>">click here to register</a> if you would like to share yours. <em>Thank you</em>.
       </p>
     </div>
+  <?php else: ?>
+    <header class="jumbotron subhead" id="overview">
+      <div class="row">
+        <div class="span12">
+          <h1>Yii Theme Factory</h1>
+        </div>
+      </div>
+    </header>
   <?php endif; ?>
 
-  <div class="span12 main-theme">
-    <a href="<?php echo Yii::app()->controller->createUrl( 'theme/view', array( 'id' => $random_five[0]->id, 'title' => Controller::makeMePretty( $random_five[0]->name ) ) ); ?>"><img src="/image.php?width=1170&height=658&cropratio=1170:658&image=/files/screenshots/<?php echo $random_five[0]->preview1; ?>"></a>
-    <div class="span3 main-theme-caption">
-      <h3><?php echo $random_five[0]->name; ?></h3>
-      <p>
-        <?php echo $random_five[0]->short_desc; ?>
-      </p>
-      <div>
-        <div class="label">Author</div> <a href="<?php echo Yii::app()->controller->createUrl( '/theme/index', array( 'artist' => $random_five[0]->user->username ) ); ?>"><?php echo $random_five[0]->user->username ?></a>
-      </div>
-      <div>
-        <div class="label">Viewed</div> <?php echo number_format( $random_five[0]->viewed ); ?>
-      </div>
-      <div>
-        <div class="label">Downloaded</div> <?php echo number_format( $random_five[0]->downloaded ); ?>
-      </div>
-      <p>
-        <div>
-          <a href="<?php echo Yii::app()->controller->createUrl( 'theme/view', array( 'id' => $random_five[0]->id, 'title' => Controller::makeMePretty( $random_five[0]->name ) ) ); ?>" class="btn btn-warning btn-small">View Theme</a>
+  <div class="span12">
+    <div class="main-theme">
+      <a href="<?php echo Yii::app()->controller->createUrl( 'theme/view', array( 'id' => $random_five[0]->id, 'title' => Controller::makeMePretty( $random_five[0]->name ) ) ); ?>"><img src="/image.php?width=1170&height=658&cropratio=1170:658&image=/files/screenshots/<?php echo $random_five[0]->preview1; ?>"></a>
+      <div class="span3">
+        <div class="main-theme-caption">
+          <h3><?php echo $random_five[0]->name; ?></h3>
+          <p>
+            <?php echo $random_five[0]->short_desc; ?>
+          </p>
+          <div>
+            <div class="label">Author</div> <a href="<?php echo Yii::app()->controller->createUrl( '/theme/index', array( 'artist' => $random_five[0]->user->username ) ); ?>"><?php echo $random_five[0]->user->username ?></a>
+          </div>
+          <div>
+            <div class="label">Viewed</div> <?php echo number_format( $random_five[0]->viewed ); ?>
+          </div>
+          <div>
+            <div class="label">Downloaded</div> <?php echo number_format( $random_five[0]->downloaded ); ?>
+          </div>
+          <p>
+            <div>
+              <a href="<?php echo Yii::app()->controller->createUrl( 'theme/view', array( 'id' => $random_five[0]->id, 'title' => Controller::makeMePretty( $random_five[0]->name ) ) ); ?>" class="btn btn-warning btn-small">View Theme</a>
+            </div>
+          </p>
         </div>
-      </p>
-    </div>
+      </div> <!-- .span3 -->
+      <div class="main-theme-bottom-caption">
+        <h5><a href="<?php echo Yii::app()->controller->createUrl( 'theme/view', array( 'id' => $random_five[0]->id, 'title' => Controller::makeMePretty( $random_five[0]->name ) ) ); ?>"><?php echo $random_five[0]->name; ?></a><span class="by-whom"> by <a href="<?php echo Yii::app()->controller->createUrl( '/theme/index', array( 'artist' => $random_five[0]->user->username ) ); ?>"><?php echo $random_five[0]->user->username; ?></a></span></h5>
+      </div> <!-- .main-theme-bottom-caption -->
+    </div> <!-- .main-theme -->
   </div> <!-- .span12 -->
 </div> <!-- .row -->
 
-<div class="">
-  <div class="ad-content">
-    <script type="text/javascript"><!--
-    google_ad_client = "ca-pub-1319358860215477";
-    /* Theme Factory - Home - Big */
-    google_ad_slot = "2915049579";
-    google_ad_width = 728;
-    google_ad_height = 90;
-    //-->
-    </script>
-    <script type="text/javascript"
-    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-    </script>
+<div class="row">
+  <div class="span12">
+    <div class="ad-content">
+      <script type="text/javascript"><!--
+      google_ad_client = "ca-pub-1319358860215477";
+      /* Theme Factory - Home - Big */
+      google_ad_slot = "2915049579";
+      google_ad_width = 728;
+      google_ad_height = 90;
+      //-->
+      </script>
+      <script type="text/javascript"
+      src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+      </script>
+    </div>
   </div>
 </div>
 
@@ -62,33 +79,39 @@
 </div>
 
 <div class="row">
-  <div class="span6 ad-content">
-    <script type="text/javascript"><!--
-    google_ad_client = "ca-pub-1319358860215477";
-    /* Theme Factory - Home - middle */
-    google_ad_slot = "5868515973";
-    google_ad_width = 468;
-    google_ad_height = 60;
-    //-->
-    </script>
-    <script type="text/javascript"
-    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-    </script>
+  <div class="span6">
+    <div class="ad-content">
+      <script type="text/javascript"><!--
+      google_ad_client = "ca-pub-1319358860215477";
+      /* Theme Factory - Home - middle */
+      google_ad_slot = "5868515973";
+      google_ad_width = 468;
+      google_ad_height = 60;
+      //-->
+      </script>
+      <script type="text/javascript"
+      src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+      </script>
+    </div>
   </div>
-  <div class="span6 ad-content">
-    <script type="text/javascript"><!--
-    google_ad_client = "ca-pub-1319358860215477";
-    /* Theme Factory - Home - middle */
-    google_ad_slot = "5868515973";
-    google_ad_width = 468;
-    google_ad_height = 60;
-    //-->
-    </script>
-    <script type="text/javascript"
-    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-    </script>  
+  <div class="span6">
+    <div class="ad-content">
+      <script type="text/javascript"><!--
+      google_ad_client = "ca-pub-1319358860215477";
+      /* Theme Factory - Home - middle */
+      google_ad_slot = "5868515973";
+      google_ad_width = 468;
+      google_ad_height = 60;
+      //-->
+      </script>
+      <script type="text/javascript"
+      src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+      </script>
+    </div>
   </div>
 </div>
+
+<p></p>
 
 <div class="row">
   <?php for ( $i = 3; $i < 5 ; $i++ ) : ?>
