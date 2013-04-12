0b8dace9691b8462a47f757a490d46e8ce52b58b
Imre Mehesz <imehesz@gmail.com>
2013-04-11 21:05:47 -0400

    layout update

--- themes/icon/views/layouts/main.php
+++ themes/icon/views/layouts/main.php
@@ -77,7 +77,7 @@
 
     <footer id="footer">
       <p>
-        <div class="label pull-right">v 3.0.<span id='VERSION_ID'>2013.0323.1232</span></div>
+        <div class="label pull-right">v 3.0.<span id='VERSION_ID'>2013.0411.2105</span></div>
       </p>
       <div class="links">
         <a href="<?php echo $this->createUrl('/site/index'); ?>">Home</a>
